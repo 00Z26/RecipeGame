46c349b974648695142ff39c2b7a25ceac27bbca
620 <lwlau@qq.com>
2023-05-12 05:52:50 +0800

    scriptableObject结构

--- Assets/Script/Dialogue/Data/DialogueData.cs
+++ Assets/Script/Dialogue/Data/DialogueData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="DialogueData1", menuName ="SO_Data")]
 public class DialogueData : ScriptableObject
 {
+    public string playerName;
     public List<DialogueStruct> dialogueList;
 
 }
@@ -13,9 +14,19 @@ public class DialogueStruct
     public int index;
     public string speakerName;
     public Sprite pic;
+    public string chatPartner;
+    public List<State> chatPartnerState;
     public string content;
     public string animiation;
     public int nextIndex;
-    public string choice;
-}
+    public string choice_1;//nextIndex : 剧情内容
+    public string choice_2;
+    public string choice_3;
 
+    //分周目生成数据，聊天对象作为一个标识保存
+}
+public class State
+{
+    public bool isAbnormal;
+    public bool isEnableChange;
+}
