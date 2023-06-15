bac46c59383155d7d51b036a1db3c6c6198cd666
620 <lwlau@qq.com>
2023-06-16 01:29:16 +0800

    行动点

--- Assets/Script/Player/Times.cs
+++ Assets/Script/Player/Times.cs
@@ -1,17 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Times : MonoBehaviour
 {
     public TMP_Text tmpText;
+    public OpenDoorTimes openDoorTimes;
+    public Image point1;
+    public Image point2;
     //进门出门
-    private void Update()
+    private void FixedUpdate()
     {
-        int currentTimes = 5 - GameObject.FindWithTag("Player").GetComponent<PhysicsCheck>().thisLoopOpenDoorTimes;
-        int showTime = 2 * currentTimes;
-        tmpText.text = showTime.ToString() + ":00";
+        int currentTimes = openDoorTimes.thisLoopDoorTimes.Sum();
+        //int currentTimes = 5 - GameObject.FindWithTag("Player").GetComponent<PhysicsCheck>().thisLoopOpenDoorTimes;
+        if(currentTimes == 1)
+        {
+            point2.gameObject.SetActive(false);
+        }
+        if(currentTimes == 2)
+        {
+            point2.gameObject.SetActive(false);
+            point1.gameObject.SetActive(false);
+            tmpText.text = "行动点已用尽，请前往1L右侧。";
+        }
+        if (currentTimes == 0)
+        {
+            tmpText.text = "行动点：";
+            point1.gameObject.SetActive(true);
+            point2.gameObject.SetActive(true);
+        }
+        
+        
+        //int showTime = 2 * currentTimes;
+        //tmpText.text = showTime.ToString() + ":00";
 
     }
 }
